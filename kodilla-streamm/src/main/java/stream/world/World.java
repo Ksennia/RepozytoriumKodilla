732085e14895fe66665faca001f3e0f65d1abe0a
nasz_login_do_github <nasz_email_do_github>
2020-03-02 15:46:29 +0100

    Naprawiono, usunełam niepotrzebne importy

--- kodilla-streamm/src/main/java/stream/world/World.java
+++ kodilla-streamm/src/main/java/stream/world/World.java
@@ -3,15 +3,13 @@ package stream.world;
 import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
-import static jdk.nashorn.internal.objects.NativeArray.reduce;
 
 public final class World {
     private final List<Continent> continents = new LinkedList<>();
 
     public BigDecimal getTotalPeopleQuantity() {
-        return continents.stream()
+        return  continents.stream()
                 .flatMap(continent -> continent.getContinents().stream())
                 .map(Country :: getPeopleQuantity)
                 .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
