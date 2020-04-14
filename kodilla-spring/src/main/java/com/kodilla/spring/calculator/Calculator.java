aff3f5ce109c9e58cc6339c02b77ca18c36f316b
nasz_login_do_github <nasz_email_do_github>
2020-04-14 18:57:37 +0200

    14.2 poprawka

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -8,32 +8,32 @@ import org.springframework.stereotype.Service;
 public class Calculator {
     @Autowired
     private Display display;
-    private double val;
+
 
 
     public double add( double a, double b) {
-        val = a + b;
+         double val = a + b;
         display.displayValue(val);
         return  val;
     }
 
 
     public double sub( double a, double b) {
-        val = a - b;
+        double val = a - b;
         display.displayValue(val);
         return a - b;
     }
 
 
     public double mul( double a, double b) {
-        val = a * b;
+        double val = a * b;
         display.displayValue(val);
         return val;
     }
 
 
     public double div( double a, double b) {
-        val = a / b;
+        double val = a / b;
         display.displayValue(val);
         return val;
     }
