743a347f9d17ef702b769fecd39154e06736831f
nasz_login_do_github <nasz_email_do_github>
2020-05-03 17:45:31 +0200

    piersha encja hibernate

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -0,0 +1,18 @@
+package com.kodilla.hibernate.task.dao;
+
+import com.kodilla.hibernate.task.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@Transactional
+public interface TaskDao extends CrudRepository<Task, Integer> {
+
+    List<Task> findByDuration(int duration);
+    List<Task> findByDescriptionAndCreated(String description, Date created);
+}
